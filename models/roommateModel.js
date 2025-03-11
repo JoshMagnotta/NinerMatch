3e8fb5b0a1505b5423f243659512f2c9c0477b7c
gavin <gwither1@charlotte.edu>
2025-03-11 15:38:52 -0400

    added in roommate data model as well as database connection, inserted one dummy item into database to check connectivity and functionality

---
+++ models/roommateModel.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const roommateSchema = new Schema({
+    title: {type: String, required: true},
+    poster: {type: String, required: true},
+    details: {type: String, minLength: [10, 'details should have at least 10 characters'], required: true},
+    },
+    {timestamps: true}
+);
+
+module.exports = mongoose.model('Roommate', roommateSchema);
